a63725493255f86ceadf84e9f941a106d42a4222
isaacvt01 <_vasqueztucent@cifpfbmoll.eu>
2023-04-15 12:06:43 +0200

    First commit

---
+++ src/main/java/edu/craptocraft/jdbcmariadb/JdbcMariaDbApplication.java
@@ -0,0 +1,13 @@
+package edu.craptocraft.jdbcmariadb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcMariaDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdbcMariaDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/craptocraft/jdbcmariadb/JdbcMariaDbApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.craptocraft.jdbcmariadb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcMariaDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
